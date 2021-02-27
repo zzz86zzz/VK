760a0770ab558f3b41e3e63b76eed488e0e4cda5
Oleg Shulga <oleg021086@gmail.com>
2021-02-27 21:07:51 +0300

    update "Manager"

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] searchPost (int id, int ownerId, String text, int count, String domain) {
+        return posts;
+    }
+    public void deletePost (int ownerId, int postId) {
+        return;
+    }
+}
